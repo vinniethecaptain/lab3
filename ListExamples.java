b938859f8205d39b2f2b1ad6524d2768f5fc9976
vinniethecaptain <v1bui@ucsd.edu>
2023-01-26 17:47:11 -0800

    bad listexamples

--- ListExamples.java
+++ ListExamples.java
@@ -3,7 +3,15 @@ import java.util.List;
 
 interface StringChecker { boolean checkString(String s); }
 
-class ListExamples {
+class ListExamples implements StringChecker {
+
+  public boolean checkString(String s) {
+    if (s.length() > 3) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
   // Returns a new list that has all the elements of the input list for which
   // the StringChecker returns true, and not the elements that return false, in
